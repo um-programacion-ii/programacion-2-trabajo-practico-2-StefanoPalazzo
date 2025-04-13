983f57a3885153f129a7e7eb79fdd89fdff8ded9
StefanoPalazzo <spalazzo@seenka.com>
2025-04-13 03:52:27 -0300

    Agrega models

---
+++ src/models/Usuario.java
@@ -0,0 +1,57 @@
+package models;
+
+public class Usuario {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+
+    public Usuario(String id, String nombre, String apellido, String correo, String contrasena) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
