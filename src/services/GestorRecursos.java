19e7838a640912a0a0527ae0fa95d8a3bb372d02
StefanoPalazzo <spalazzo@seenka.com>
2025-04-15 19:08:48 -0300

    Implementa notificaciones en Gestores

--- src/services/GestorRecursos.java
+++ src/services/GestorRecursos.java
@@ -1,4 +1,36 @@
 package services;
 
+import interfaces.IServicioNotificaciones;
+import models.RecursoDigital;
+
 public class GestorRecursos {
+    private IServicioNotificaciones servicioNotificaciones;
+
+    public GestorRecursos(IServicioNotificaciones servicioNotificaciones) {
+        this.servicioNotificaciones = servicioNotificaciones;
+    }
+
+    public void listarRecursos() {
+        servicioNotificaciones.enviarNotificacion("Listado de recursos mostrado correctamente.");
+    }
+
+    public void agregarRecurso(RecursoDigital r) {
+        servicioNotificaciones.enviarNotificacion("RecursoDigital " + r.getTitulo() + " agregado con éxito.");
+    }
+
+    public void eliminarRecurso(RecursoDigital r) {
+        servicioNotificaciones.enviarNotificacion("RecursoDigital " + r.getTitulo() + " eliminado con éxito.");
+    }
+
+    public void prestarRecurso(RecursoDigital r) {
+        servicioNotificaciones.enviarNotificacion("RecursoDigital " + r.getTitulo() + " prestado con éxito.");
+    }
+
+    public void devolverRecurso(RecursoDigital r) {
+        servicioNotificaciones.enviarNotificacion("RecursoDigital " + r.getTitulo() + " devuelto con éxito.");
+    }
+
+    public void renovarRecurso(RecursoDigital r) {
+        servicioNotificaciones.enviarNotificacion("RecursoDigital " + r.getTitulo() + " renovado con éxito.");
+    }
 }
